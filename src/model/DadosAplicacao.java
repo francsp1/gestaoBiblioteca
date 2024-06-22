92672ac66df3577af2ac08132a5073f893eade55
Francisco Pedrosa <francsp1@hotmail.com>
2024-06-22 17:39:25 +0100

    adicionarSocio()

--- src/model/DadosAplicacao.java
+++ src/model/DadosAplicacao.java
@@ -5,14 +5,11 @@ import java.util.ArrayList;
 public enum DadosAplicacao {
     INSTANCIA;
 
-    private ArrayList<Fornecedor> fornecedores;
-    private ArrayList<Livro> livros;
-    private ArrayList<Socio> socios;
+    private ArrayList<Fornecedor> fornecedores = new ArrayList<>();
+    private ArrayList<Livro> livros = new ArrayList<>();
+    private ArrayList<Socio> socios = new ArrayList<>();
 
     private DadosAplicacao() {
-        this.fornecedores = new ArrayList<>();
-        this.livros = new ArrayList<>();
-
         Fornecedor fornecedor1 = new Fornecedor("PortoEditora", "DHL", "244 2444 244");
         Fornecedor fornecedor2 = new Fornecedor("Livros Horizonte", "DHL", "244 2444 244");
         Fornecedor fornecedor3 = new Fornecedor("Leya", "DHL", "244 2444 244");
@@ -51,7 +48,19 @@ public enum DadosAplicacao {
         adicionarLivro(livro4);
         adicionarLivro(livro6);
 
+        Socio socio1 = new Socio("João", 123456789, "Rua do João", 912345678, "joao@mail.com", MetodoComunicacaoPreferido.EMAIL);
+        Socio socio2 = new Socio("Maria", 987654321, "Rua da Maria", 912345678, "maria@mail.com", MetodoComunicacaoPreferido.EMAIL);
+        Socio socio3 = new Socio("José", 123456789, "Rua do José", 912345678, "jose@mail.com", MetodoComunicacaoPreferido.EMAIL);
+        Socio socio4 = new Socio("Ana", 987654321, "Rua da Ana", 912345678, "ana@mail.com", MetodoComunicacaoPreferido.SMS);
+        Socio socio5 = new Socio("Manuel", 123456789, "Rua do Manuel", 912345678, "manuel@mail.com", MetodoComunicacaoPreferido.SMS);
+        Socio socio6 = new Socio("Marta", 987654321, "Rua da Marta", 912345678, "marta@mail.com", MetodoComunicacaoPreferido.SMS);
 
+        adicionarSocio(socio1);
+        adicionarSocio(socio2);
+        adicionarSocio(socio3);
+        adicionarSocio(socio4);
+        adicionarSocio(socio5);
+        adicionarSocio(socio6);
     }
 
     public void adicionarFornecedor(Fornecedor fornecedor) {
@@ -68,6 +77,10 @@ public enum DadosAplicacao {
 
     public ArrayList<Livro> getLivros() {
         return livros;
+    }
+
+    public void adicionarSocio(Socio socio) {
+        socios.add(socio);
     }
 
     public ArrayList<Socio> getSociosAtivos() {
